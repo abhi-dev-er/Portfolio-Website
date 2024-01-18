9f1c531a6fca5b296ec1e24a570e1e7bc4da5b2f
abhi-dev-er <abhishekdubeysatna55@gmail.com>
2024-01-18 12:53:52 +0530

    creating hero section

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,34 +1,51 @@
 import React from "react";
 import hero from "../../assets/hero.jpeg";
-import { Button, Link } from "react-scroll";
-import { BsLinkedin, BsGithub,} from "react-icons/bs"
+import { Link } from "react-scroll";
+import { BsLinkedin, BsGithub } from "react-icons/bs";
 
 function Home() {
   return (
-    <section className="">
-      <div className="">
-        <span>
+    <section className="h-[90vh] w-full max-w-6xl mt-4 mb-0 overflow-hidden ml-auto mr-auto relative">
+      <div className="h-[100vh] w-[100vw] p-8 text-xl font-semibold font-serif flex flex-col  justify-center ">
+        <span className="text-[1.75rem] font-thin">
           Hello, I'm <br />
-          <span>Abhishek Dubey</span>
+          <span className=" text-yellow-600">Abhishek Dubey</span>
         </span>
         <h1>
           {" "}
-          <span>Front-End Web Developer</span>
+          <span className="text-[1.5rem]">Front-End Web Developer</span>
         </h1>
-        <p>
-          Im a Skilled and Passionate front-end web Developer <br /> with experience  in
-          creating visually appealing and user-friendly website{" "}
+        <p className="text-sm font-thin">
+          Im a Skilled and Passionate front-end web Developer <br /> with
+          experience in creating visually appealing and user-friendly website{" "}
         </p>
-        <Link><button>Download CV</button></Link> 
-        <Link><button>Hire me</button></Link>
-        <div>
-            <span><BsGithub/></span>
-            <span><BsLinkedin/></span>
-            
+        <div className="flex gap-2 mt-4">
+          <Link>
+            <button className="py-2 px-4 bg-blue-700 text-white rounded-2xl text-xs hover:bg-black transition-all ease-in-out duration-300">
+              Download CV
+            </button>
+          </Link>
+          <Link>
+            <button className="py-2 px-4 bg-blue-700 text-white rounded-2xl text-xs hover:bg-black transition-all ease-in-out duration-300">
+              Hire me
+            </button>
+          </Link>
+        </div>
+        <div className="flex gap-2 mt-4">
+          <span>
+            <BsGithub />
+          </span>
+          <span>
+            <BsLinkedin />
+          </span>
         </div>
       </div>
-      <div>
-        <img src={hero} alt="" />
+      <div className="">
+        <img
+          className="rounded-[50%] w-[400px] h-[400px] absolute -z-[1] top-[3.5rem] right-[0] "
+          src={hero}
+          alt=""
+        />
       </div>
     </section>
   );
