4969824d8f95930aa0e5067b539667cb73230bc9
abhi-dev-er <abhishekdubeysatna55@gmail.com>
2024-01-20 09:29:53 +0530

    adding about section

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -3,32 +3,39 @@ import myImg from "../../assets/hero.jpeg";
 
 function About() {
   return (
-    <div className="w-full h-[100vh]">
+    <div className="w-full h-[100vh] ">
       <h1 className="flex justify-center items-center font-serif text-[2.5rem] font-semibold p-14">
         About Me
       </h1>
-      <div className="flex flex-row max-w-6xl w-[100vw] h-[100vh] p-12">
-        <div className="">
-          <img src="" alt="" />
+      <div className="flex flex-row max-w-6xl w-[100vw] h-[60vh] p-12 ml-24 mt-8">
+        <div className="w-[50%] h-[300px]">
+          <img
+            className="object-cover overflow-hidden w-[100%] rounded-lg h-[300px]"
+            src={myImg}
+            alt=""
+          />
         </div>
-        <div>
-          <div>
-            <h3>Education</h3>
+        <div className="ml-24 w-[70%]">
+          <div className="font-serif">
+            <h3 className="text-2xl font-serif font-bold">Education : -</h3>
             <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi,
-              porro!
+              Bachelor of Computer Science{" "}
+              <span className="text-green-500">Year : 2023</span>
             </p>
             <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi,
-              porro!
+              Diploma in Computer Science & Engineering{" "}
+              <span className="text-green-500">Year : 2020</span>
             </p>
-            <h3>Experience</h3>
-            <p>Lorem ipsum dolor sit.</p>
+            <h3 className="text-2xl font-serif font-bold">Experience : -</h3>
+            <p>6 Month Front-End Web Development </p>
           </div>
-          <p>
+          <p className="mt-4 leading-6">
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis
             odit tenetur minima eum temporibus exercitationem, repudiandae,
-            earum neque delectus ipsum quisquam ipsam modi.
+            earum neque delectus ipsum quisquam ipsam modi. Lorem ipsum dolor
+            sit amet consectetur adipisicing elit. Tenetur, in repellat earum
+            quo debitis quaerat ullam nulla eaque repellendus recusandae sed
+            dolores suscipit!
           </p>
         </div>
       </div>
